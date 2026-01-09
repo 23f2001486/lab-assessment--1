865adede51f717e06921b0292d11b0c9bc3f61c0
matlab <matlab@sjt418scope060>
2026-01-09 08:21:11 +0530

    lab assessment done

---
+++ labassessment/src/labassessment/HelloWorld.java
@@ -0,0 +1,8 @@
+package labassessment;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Lab Assessment -1 Divya P 23MIC0043");
+	}
+}
